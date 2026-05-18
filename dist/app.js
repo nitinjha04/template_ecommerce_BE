720f168ecb75537e2270b86f37cae72a9ef33f67
nitinjha04 <nitinjha890@gmail.com>
2026-05-18 22:59:42 +0530

    cors updated

--- dist/app.js
+++ dist/app.js
@@ -11,15 +11,26 @@ const env_1 = require("./config/env");
 const error_middleware_1 = require("./middleware/error.middleware");
 const routes_1 = __importDefault(require("./routes"));
 const app = (0, express_1.default)();
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://template-ecommerce-fe.vercel.app",
+];
 app.use((0, helmet_1.default)());
 app.use((0, cors_1.default)({
-    origin: env_1.env.corsOrigin,
+    origin: function (origin, callback) {
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
 }));
-app.use((0, morgan_1.default)(env_1.env.nodeEnv === 'development' ? 'dev' : 'combined'));
-app.use(express_1.default.json({ limit: '10mb' }));
+app.use((0, morgan_1.default)(env_1.env.nodeEnv === "development" ? "dev" : "combined"));
+app.use(express_1.default.json({ limit: "10mb" }));
 app.use(express_1.default.urlencoded({ extended: true }));
-app.use('/api/v1', routes_1.default);
+app.use("/api/v1", routes_1.default);
 app.use(error_middleware_1.notFound);
 app.use(error_middleware_1.errorHandler);
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,36 @@
-import cors from 'cors';
-import express from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
-import { env } from './config/env';
-import { errorHandler, notFound } from './middleware/error.middleware';
-import routes from './routes';
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import { env } from "./config/env";
+import { errorHandler, notFound } from "./middleware/error.middleware";
+import routes from "./routes";
 
 const app = express();
-
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://template-ecommerce-fe.vercel.app",
+];
 app.use(helmet());
 app.use(
   cors({
-    origin: env.corsOrigin,
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
     credentials: true,
-  })
+  }),
 );
-app.use(morgan(env.nodeEnv === 'development' ? 'dev' : 'combined'));
-app.use(express.json({ limit: '10mb' }));
+app.use(morgan(env.nodeEnv === "development" ? "dev" : "combined"));
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 app.use(notFound);
 app.use(errorHandler);
